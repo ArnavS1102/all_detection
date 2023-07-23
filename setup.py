2c51c444c14274df9b85ef12e00e1df7cf4bd9c9
ArnavS1102 <arnsharma02@gmail.com>
2023-07-23 22:29:06 +0530

    sETUP CHANGES1

--- setup.py
+++ setup.py
@@ -1,12 +1,8 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name="alldetection",
     version="0.1.1",
-    packages=find_packages(),
-    install_requires=[
-        "tensorflow"
-    ],
     author="Arnav S",
     description="A package containing the VIT CLASSIFIER",
     url="https://github.com/ArnavS1102/all_detection.git"
