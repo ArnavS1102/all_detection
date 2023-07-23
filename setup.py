725f883c153be597b3c78564b2816b6a65e2a779
ArnavS1102 <arnsharma02@gmail.com>
2023-07-23 19:50:25 +0530

    Changed setup config4

--- setup.py
+++ setup.py
@@ -3,9 +3,5 @@ from setuptools import find_packages,setup
 setup(name="alldetection",
     version="1.0",
     install_requires=[],
-    packages=find_packages(exclude="notebooks"),
-    extras_require={
-        "all": ["matplotlib", "pycocotools", "opencv-python", "onnx", "onnxruntime"],
-        "dev": ["flake8", "isort", "black", "mypy"],
-    },
+    packages=find_packages(exclude="notebooks")
 )
